7eef521a78d3d4826c3fdc58fa6f798f6149bdad
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-03-24 13:14:18 +0000

    Remove reference to svb code

--- varbay/__init__.py
+++ varbay/__init__.py
@@ -7,13 +7,11 @@ except ImportError:
     __version__ = "Unknown version"
     __timestamp__ = "Unknown timestamp"
 
-from .svb import SvbFit
 from .data import DataModel
 from .model import Model, get_model_class
 
 __all__ = [   "__version__",
     "__timestamp__",
-    "SvbFit",
     "DataModel",
     "Model",
     "get_model_class",

43e930478bc9e8e522a4e8e8827da3f170e48106
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-04-29 15:12:18 +0100

    Add laplacian calculation from svb in order to allow implementation of spatial prior

--- vaby/data.py
+++ vaby/data.py
@@ -7,6 +7,7 @@ import collections
 import six
 import numpy as np
 import nibabel as nib
+from scipy import sparse
 
 from .utils import LogBase
 
@@ -31,7 +32,7 @@ class DataModel(LogBase):
 
         # If there is a mask load it and use it to mask the data
         if mask is not None:
-            mask_nii, self.mask_vol = self._get_data(mask)
+            _mask_nii, self.mask_vol = self._get_data(mask)
             if self.shape != list(self.mask_vol.shape):
                 raise ValueError("Mask has different shape to main data: %s vs %s" % (self.shape, self.mask_vol.shape))
             self.mask_flattened = self.mask_vol.flatten()
@@ -48,39 +49,101 @@ class DataModel(LogBase):
         else:
             self.post_init = None
 
-        self._calc_neighbours()
-    
-    def nodes_to_voxels_ts(self, tensor, edge_scale=True):
-        return tensor
+        self._calc_adjacency_matrix()
+        self._calc_laplacian()
 
-    def nodes_to_voxels(self, tensor, edge_scale=True):
-        return tensor
-
-    def voxels_to_nodes(self, tensor, edge_scale=True):
-        return tensor
-
-    def voxels_to_nodes_ts(self, tensor, edge_scale=True):
-        return tensor
-
-    def vertices_to_voxels(self, tensor, vertex_axis=0):
+    def _calc_laplacian(self):
         """
-        Map parameter vertex-based data to data voxels
-
-        This is for the use case where data is defined in a different space to the
-        parameter estimation space. For example we may be estimating parameters on
-        a surface mesh, but using volumetric data to train this model.
-
-        FIXME For now we simply have a default case where the two spaces are identical.
-
-        :param tensor: TensorFlow tensor of which one axis represents indexing over
-                       parameter vertices
-        :param vertex_axis: Index of axis of tensor which corresponds to parameter vertices
-
-        :return: TensorFlow tensor with parameter vertex axis replaced by data voxel
-                 axis and other tensor entries transformed appropriately to the 
-                 data space
+        Laplacian matrix. Note the sign convention is negatives
+        on the diagonal, and positive values off diagonal. 
         """
+
+        # Set the laplacian here 
+        lap = self.adj_matrix.todok(copy=True)
+        lap[np.diag_indices(lap.shape[0])] = -lap.sum(1).T
+        assert lap.sum(1).max() == 0, 'Unweighted Laplacian matrix'
+        self.laplacian = lap.tocoo()
+
+    def nodes_to_voxels_ts(self, tensor, pv_sum=True):
         return tensor
+
+    def nodes_to_voxels(self, tensor, pv_sum=True):
+        return tensor
+
+    def voxels_to_nodes(self, tensor, pv_sum=True):
+        return tensor
+
+    def voxels_to_nodes_ts(self, tensor, pv_sum=True):
+        return tensor
+
+    def _calc_adjacency_matrix(self):
+        """
+        Generate adjacency matrix for voxel nearest neighbours.
+        Note the result will be a square sparse COO matrix of size 
+        (n_unmasked voxels), indexed according to voxels in the mask
+        (so index 0 refers to the first un-masked voxel). 
+        
+        These are required for spatial priors and in practice do not
+        take long to calculate so we provide them as a matter of course
+        """
+        def add_if_unmasked(x, y, z, masked_indices, nns):
+            # Check that potential neighbour is not masked and if so
+            # add it to the list of nearest neighbours
+            idx  = masked_indices[x, y, z]
+            if idx >= 0:
+                nns.append(idx)
+
+        # Generate a Numpy array which contains -1 for voxels which
+        # are not in the mask, and for those which are contains the
+        # voxel index, starting at 0 and ordered in row-major ordering
+        # Note that the indices are for unmasked voxels only so 0 is
+        # the index of the first unmasked voxel, 1 the second, etc.
+        # Note that Numpy uses (by default) C-style row-major ordering
+        # for voxel indices so the the Z co-ordinate varies fastest
+        masked_indices = np.full(self.shape, -1, dtype=np.int)
+        nx, ny, nz = tuple(self.shape)
+        voxel_idx = 0
+        for x in range(nx):
+            for y in range(ny):
+                for z in range(nz):
+                    if self.mask_vol[x, y, z] > 0:
+                        masked_indices[x, y, z] = voxel_idx
+                        voxel_idx += 1
+
+        # Now generate the nearest neighbour lists.
+        voxel_nns = []
+        indices_nn = []
+        voxel_idx = 0
+        for x in range(nx):
+            for y in range(ny):
+                for z in range(nz):
+                    if self.mask_vol[x, y, z] > 0:
+                        nns = []
+                        if x > 0: add_if_unmasked(x-1, y, z, masked_indices, nns)
+                        if x < nx-1: add_if_unmasked(x+1, y, z, masked_indices, nns)
+                        if y > 0: add_if_unmasked(x, y-1, z, masked_indices, nns)
+                        if y < ny-1: add_if_unmasked(x, y+1, z, masked_indices, nns)
+                        if z > 0: add_if_unmasked(x, y, z-1, masked_indices, nns)
+                        if z < nz-1: add_if_unmasked(x, y, z+1, masked_indices, nns)
+                        voxel_nns.append(nns)
+                        # For TensorFlow sparse tensor
+                        for nn in nns:
+                            indices_nn.append([voxel_idx, nn])
+                        voxel_idx += 1
+
+        # Edge case of no neighbours (e.g. single voxel often used in testing)
+        if len(indices_nn) == 0:
+            values = ([], [[], []])
+        else:
+            values = (np.ones(len(indices_nn)), (np.array(indices_nn).T))
+
+        self.adj_matrix = sparse.coo_matrix(
+            values,
+            shape=2*[self.n_unmasked_voxels], 
+            dtype=np.float32
+        )
+
+        assert not (self.adj_matrix.tocsr()[np.diag_indices(self.n_unmasked_voxels)] != 0).max()
 
     def nifti_image(self, data):
         """
@@ -177,68 +240,3 @@ class DataModel(LogBase):
 
         self.log.info("Posterior mean shape: %s, cov shape: %s", mean.shape, cov.shape)
         return mean, cov
-
-    def _calc_neighbours(self):
-        """
-        Generate nearest neighbour and second nearest neighbour lists
-        
-        These are required for spatial priors and in practice do not
-        take long to calculate so we provide them as a matter of course
-
-        FIXME this needs to be done in parameter space where that differs 
-        from the data space
-        """
-        def add_if_unmasked(x, y, z, masked_indices, nns):
-            # Check that potential neighbour is not masked and if so
-            # add it to the list of nearest neighbours
-            idx  = masked_indices[x, y, z]
-            if idx >= 0:
-                nns.append(idx)
-
-        # Generate a Numpy array which contains -1 for voxels which
-        # are not in the mask, and for those which are contains the
-        # voxel index, starting at 0 and ordered in row-major ordering
-        # Note that the indices are for unmasked voxels only so 0 is
-        # the index of the first unmasked voxel, 1 the second, etc.
-        # Note that Numpy uses (by default) C-style row-major ordering
-        # for voxel indices so the the Z co-ordinate varies fastest
-        masked_indices = np.full(self.shape, -1, dtype=np.int)
-        nx, ny, nz = tuple(self.shape)
-        voxel_idx = 0
-        for x in range(nx):
-            for y in range(ny):
-                for z in range(nz):
-                    if self.mask_vol[x, y, z] > 0:
-                        masked_indices[x, y, z] = voxel_idx
-                        voxel_idx += 1
-
-        # Now generate the nearest neighbour lists.
-        voxel_nns = []
-        self.indices_nn = []
-        voxel_idx = 0
-        for x in range(nx):
-            for y in range(ny):
-                for z in range(nz):
-                    if self.mask_vol[x, y, z] > 0:
-                        nns = []
-                        if x > 0: add_if_unmasked(x-1, y, z, masked_indices, nns)
-                        if x < nx-1: add_if_unmasked(x+1, y, z, masked_indices, nns)
-                        if y > 0: add_if_unmasked(x, y-1, z, masked_indices, nns)
-                        if y < ny-1: add_if_unmasked(x, y+1, z, masked_indices, nns)
-                        if z > 0: add_if_unmasked(x, y, z-1, masked_indices, nns)
-                        if z < nz-1: add_if_unmasked(x, y, z+1, masked_indices, nns)
-                        voxel_nns.append(nns)
-                        # For TensorFlow sparse tensor
-                        for nn in nns:
-                            self.indices_nn.append([voxel_idx, nn])
-                        voxel_idx += 1
-
-        # Second nearest neighbour lists exclude self but include duplicates
-        voxel_n2s = [[] for voxel in voxel_nns]
-        self.indices_n2 = []
-        for voxel_idx, nns in enumerate(voxel_nns):
-            for nn in nns:
-                voxel_n2s[voxel_idx].extend(voxel_nns[nn])
-            voxel_n2s[voxel_idx] = [v for v in voxel_n2s[voxel_idx] if v != voxel_idx]
-            for n2 in voxel_n2s[voxel_idx]:
-                self.indices_n2.append([voxel_idx, n2])

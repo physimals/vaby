a7497cc541314c679b274a3b010c6cdb13053ff2
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-07-23 10:01:21 +0100

    Updated docstrings

--- vaby/parameter.py
+++ vaby/parameter.py
@@ -1,11 +1,8 @@
 """
-SVB - Model parameters
+VABY - Model parameters
 
-This module defines a set of classes of model parameters.
-
-The factory methods which create priors/posteriors can
-make use of the instance class to create the appropriate
-type of vertexwise prior/posterior
+A parameter is a variable in the model which can be fitted to the data.
+It has a prior distribution and an initial posterior. 
 """
 import tensorflow as tf
    
@@ -15,6 +12,19 @@ from . import dist
 def get_parameter(name, **kwargs):
     """
     Factory method to create an instance of a parameter
+
+    Keyword arguments:
+         - ``prior`` Name of the prior distribution. Other keyword arguments beginning with ``prior``
+                     are passed to the ``dist.get_dist`` factory function to construct the prior
+                     distribution.
+         - ``prior_type`` Code letter giving type of prior to be used, e.g. normal, spatial, ARD...
+         - ``post`` Name of the posterior distribution. Other keyword arguments beginning with ``post``
+                     are passed to the ``dist.get_dist`` factory function to construct the posterior
+                     distribution.
+         - ``post_type`` Type of posterior, e.g. nodewise, global...
+         - ``param_overrides`` Dictionary keyed by parameter name. If a key for this parameter is found in
+                               the ``param_overrides`` dictionary, it's value will be taken as an additional
+                               dictionary of keyword arguments to override those defined in ``**kwargs``
     """
     custom_kwargs = kwargs.pop("param_overrides", {}).get(name, {})
     kwargs.update(custom_kwargs)
@@ -26,6 +36,10 @@ def get_parameter(name, **kwargs):
     post_dist = dist.get_dist(prefix="post", **kwargs)
     post_type = kwargs.get("post_type", "vertexwise")
     post_init = kwargs.get("post_init", None)
+
+    # FIXME: hack because when var = 1 the LogNormal gives inf latent cost during training
+    if (kwargs.get('dist') == 'LogNormal') and (kwargs.get('var') == 1.0):
+        raise RuntimeError('LogNormal distribution cannot have initial var = 1.0') 
 
     return Parameter(name, desc=desc, prior=prior_dist, prior_type=prior_type, post=post_dist, post_init=post_init, post_type=post_type)
 
@@ -39,17 +53,15 @@ class Parameter(LogBase):
         Constructor
 
         :param name: Parameter name
-        :param prior: Dist instance giving the parameter's prior distribution
-        :param desc: Optional parameter description
 
         Keyword arguments (optional):
-         - ``mean_init`` Initial value for the posterior mean either as a numeric
-                         value or a callable which takes the parameters t, data, param_name
-         - ``log_var_init`` Initial value for the posterior log variance either as a numeric
-                            value or a callable which takes the parameters t, data, param_name
-         - ``param_overrides`` Dictionary keyed by parameter name. Value should be dictionary
-                               of keyword arguments which will override those defined as
-                               existing keyword arguments
+         - ``desc`` Text description
+         - ``prior`` ``Dist`` instance giving the parameter's prior distribution
+         - ``prior_type`` Code letter giving type of prior to be used, e.g. normal, spatial, ARD...
+         - ``post`` ``Dist`` instance giving the parameter's initial posterior distribution. The parameters
+                    of the posterior will be inferred however the distibution type is fixed
+         - ``post_init`` Callable which will be used to initialize the posterior from the data
+         - ``post_type`` Type of posterior, e.g. nodewise, global...
         """
         LogBase.__init__(self)
 
@@ -61,8 +73,8 @@ class Parameter(LogBase):
         self.prior_dist = kwargs.get("prior")
         self.prior_type = kwargs.get("prior_type", "N")
         self.post_dist = kwargs.get("post", self.prior_dist)
-        self.post_type = kwargs.get("post_type", "vertexwise")
         self.post_init = kwargs.get("post_init", None)
+        self.post_type = kwargs.get("post_type", "vertexwise")
 
     def __str__(self):
-        return "Parameter: %s" % self.name
+        return "Parameter: %s (%s)" % (self.name, self.desc)

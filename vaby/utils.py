557349d41fa0ac796edbfd64ae4d306b91348f37
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-10-25 11:10:05 +0100

    Move logging and runtime functions into the core as useful for avb and svb

--- vaby/utils.py
+++ vaby/utils.py
@@ -2,6 +2,8 @@
 VABY - General utility functions
 """
 import logging
+import logging.config
+import os
 
 import numpy as np
 import tensorflow as tf
@@ -23,6 +25,52 @@ class LogBase(object):
     """
     def __init__(self, **kwargs):
         self.log = logging.getLogger(type(self).__name__)
+
+def setup_logging(outdir=".", **kwargs):
+    """
+    Set the log level, formatters and output streams for the logging output
+
+    By default this goes to <outdir>/logfile at level INFO
+    """
+    # First we clear all loggers from previous runs
+    for logger_name in list(logging.Logger.manager.loggerDict.keys()) + ['']:
+        logger = logging.getLogger(logger_name)
+        logger.handlers = []
+
+    if kwargs.get("log_config", None):
+        # User can supply a logging config file which overrides everything else
+        logging.config.fileConfig(kwargs["log_config"])
+    else:
+        # Set log level on the root logger to allow for the possibility of 
+        # debug logging on individual loggers
+        level = kwargs.get("log_level", "info")
+        if not level:
+            level = "info"
+        level = getattr(logging, level.upper(), logging.INFO)
+        logging.getLogger().setLevel(level)
+
+        if kwargs.get("save_log", False):
+            # Send the log to an output logfile
+            logfile = os.path.join(outdir, "logfile")
+            logging.basicConfig(filename=logfile, filemode="w", level=level)
+
+        if kwargs.get("log_stream", None) is not None:
+            # Can also supply a stream to send log output to as well (e.g. sys.stdout)
+            extra_handler = logging.StreamHandler(kwargs["log_stream"])
+            extra_handler.setFormatter(logging.Formatter('%(levelname)s : %(message)s'))
+            logging.getLogger().addHandler(extra_handler)
+
+def runtime(runnable, *args, **kwargs):
+    """
+    Record how long it took to run something
+
+    :return: Tuple of runtime (s), normal return value
+    """
+    import time
+    start_time = time.time()
+    ret = runnable(*args, **kwargs)
+    end_time = time.time()
+    return (end_time - start_time), ret
 
 def scipy_to_tf_sparse(scipy_sparse):
     """Converts a scipy sparse matrix to TF representation"""
